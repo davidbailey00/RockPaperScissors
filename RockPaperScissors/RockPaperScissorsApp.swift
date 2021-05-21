b69cb6ce5b172e8df99a2918243fe63d02dbddae
David Bailey <davidbailey00@outlook.com>
2021-05-21 13:19:37 +0100

    initial commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by David Bailey on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
